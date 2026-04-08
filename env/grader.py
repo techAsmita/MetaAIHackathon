baca92916065467ddde5876b8376d2e57a22412b
Asmita Roy <142912385+techAsmita@users.noreply.github.com>
2026-04-08 17:21:22 +0530

    Update grading logic to use float for score
    Changed score from integer to float and added critical fix for score limits.

--- env/grader.py
+++ env/grader.py
@@ -1,12 +1,21 @@
 def grade(response):
     response = response.lower()
 
-    score = 0
+    score = 0.0
+
     if "sorry" in response:
         score += 0.3
+
     if "refund" in response:
         score += 0.4
+
     if len(response) > 20:
         score += 0.3
+
+    # CRITICAL FIX
+    if score >= 1.0:
+        score = 0.95
+    elif score <= 0.0:
+        score = 0.05
 
     return score
