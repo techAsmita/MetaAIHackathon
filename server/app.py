ab09961884b9ab17b2ad6fefdf364bdb478d8c48
Asmita Roy <142912385+techAsmita@users.noreply.github.com>
2026-04-08 13:52:46 +0530

    Remove FastAPI server initialization code

--- server/app.py
+++ server/app.py
@@ -2,11 +2,3 @@ import os
 
 if os.getenv("RUN_ENV") == "runner":
     exit()
-
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def root():
-    return {"message": "Server running"}
