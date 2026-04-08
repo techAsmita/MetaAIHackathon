fa3c81a11609dcffecf02c98c8a71e6399584b2c
Asmita Roy <142912385+techAsmita@users.noreply.github.com>
2026-04-08 14:38:41 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -1,4 +1,18 @@
-import os
+from fastapi import FastAPI
 
-if os.getenv("RUN_ENV") == "runner":
-    exit()
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Server running"}
+
+
+# REQUIRED FOR OPENENV VALIDATION
+def main():
+    import uvicorn
+    uvicorn.run("server.app:app", host="0.0.0.0", port=8000)
+
+
+# REQUIRED ENTRY POINT
+if __name__ == "__main__":
+    main()
